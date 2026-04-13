628c4ff4dca78fb0dd15e14698bf2ef88f0928e3
andrinruegg <andrin.rueegg@edu.tbz.ch>
2026-04-13 16:05:14 +0200

    refactor: remove legacy theme context and toggle component to streamline application state

--- Roulette/src/App.tsx
+++ Roulette/src/App.tsx
@@ -7,27 +7,24 @@ import SlotsGame from './components/organisms/SlotsGame';
 import MusicPlayer from './components/molecules/MusicPlayer';
 import Deposit from './components/pages/Deposit';
 import AppLayout from './components/templates/AppLayout';
-import { ThemeProvider } from './context/ThemeContext';
 import { BalanceProvider } from './context/BalanceContext';
 
 const App: React.FC = () => {
     return (
-        <ThemeProvider>
-            <BalanceProvider>
-                <BrowserRouter>
-                    <MusicPlayer />
-                    <AppLayout>
-                        <Routes>
-                            <Route path="/" element={<Lobby />} />
-                            <Route path="/roulette" element={<RouletteGame />} />
-                            <Route path="/blackjack" element={<BlackjackGame />} />
-                            <Route path="/slots" element={<SlotsGame />} />
-                            <Route path="/deposit" element={<Deposit />} />
-                        </Routes>
-                    </AppLayout>
-                </BrowserRouter>
-            </BalanceProvider>
-        </ThemeProvider>
+        <BalanceProvider>
+            <BrowserRouter>
+                <MusicPlayer />
+                <AppLayout>
+                    <Routes>
+                        <Route path="/" element={<Lobby />} />
+                        <Route path="/roulette" element={<RouletteGame />} />
+                        <Route path="/blackjack" element={<BlackjackGame />} />
+                        <Route path="/slots" element={<SlotsGame />} />
+                        <Route path="/deposit" element={<Deposit />} />
+                    </Routes>
+                </AppLayout>
+            </BrowserRouter>
+        </BalanceProvider>
     );
 };
 
